5758f9e574cf1bdd5df6fff717516132d242f558
Ilya_Zuev_90 <god_of_iwowi@mail.ru>
2023-05-25 17:57:27 +0400

    Added Main.java

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 21_110;
+        int bonusPrice = 20;
+        int bonusMiles = ticketPrice / bonusPrice;
+
+        System.out.println(bonusMiles);
+       
+    }
+}
